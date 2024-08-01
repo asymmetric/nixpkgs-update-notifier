f6bf68129b720161d8eb38b60463978491a0354e
Lorenzo Manacorda <lorenzo@mailbox.org>
2024-08-01 21:19:54 +0200

    wip

--- go.mod
+++ go.mod
@@ -1,19 +1,35 @@
 module example.com/go-hello
 
-go 1.16
+go 1.22.3
+
+toolchain go1.22.5
+
+require (
+	github.com/gocolly/colly v1.2.0
+	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/velebak/colly-sqlite3-storage v0.0.0-20240410181914-45e8d740b550
+)
 
 require (
 	github.com/PuerkitoBio/goquery v1.9.2 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
 	github.com/antchfx/htmlquery v1.3.2 // indirect
 	github.com/antchfx/xmlquery v1.4.1 // indirect
+	github.com/antchfx/xpath v1.3.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/gocolly/colly v1.2.0 // indirect
-	github.com/jawher/mow.cli v1.2.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
-	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
-	github.com/velebak/colly-sqlite3-storage v0.0.0-20240410181914-45e8d740b550 // indirect
 	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
+	gopkg.in/yaml.v2 v2.2.5 // indirect
 )
+
+replace github.com/velebak/colly-sqlite3-storage => /home/asymmetric/code/colly-sqlite3-storage
--- main.go
+++ main.go
@@ -1,22 +1,54 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"hash/fnv"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"io"
 
 	"github.com/gocolly/colly"
 	"github.com/velebak/colly-sqlite3-storage/colly/sqlite3"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
 	fmt.Println("Hello Nix!")
 	url := "https://nixpkgs-update-logs.nix-community.org/"
+	filename := "data.sql"
+
+	db, err := sql.Open("sqlite3", filename)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	statement, _ := db.Prepare("CREATE TABLE IF NOT EXISTS visited (id INTEGER PRIMARY KEY, pathID INTEGER) STRICT")
+	_, err = statement.Exec()
+	if err != nil {
+		panic(err)
+	}
+
+	statement, _ = db.Prepare("CREATE INDEX IF NOT EXISTS idx_visited ON visited (pathID)")
+	_, err = statement.Exec()
+	if err != nil {
+		panic(err)
+	}
 
 	c := colly.NewCollector(
 		colly.UserAgent("asymmetric"),
-		// FIXME
 		// colly.AllowedDomains(url),
 		// colly.Debugger(&debug.LogDebugger{}),
-		colly.AllowURLRevisit(),
+		// colly.AllowURLRevisit(),
 	)
 
 	storage := &sqlite3.Storage{
@@ -25,14 +57,14 @@ func main() {
 
 	defer storage.Close()
 
-	err := c.SetStorage(storage)
-	if err != nil {
-		panic(err)
-	}
+	// err := c.SetStorage(storage)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
+	// c.OnRequest(func(r *colly.Request) {
+	// 	fmt.Println("Visiting", r.URL.String())
+	// })
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
 
@@ -41,21 +73,75 @@ func main() {
 			return
 		}
 
-		// fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-		// ok, err := storage.IsVisited(e.Request.ID)
-		// if err != nil {
-		// 	panic(err)
-		// }
-		//
-		// if ok {
-		// 	fmt.Println("Already visited: %s", link)
-		// 	return
-		// }
+		match, err := regexp.MatchString(`\.log$`, link)
+		if err != nil {
+			panic(err)
+		}
+
+		fullpath := e.Request.AbsoluteURL(link)
+
+		// if it's a link to a log file:
+		// - did we already know this file?
+		// - download file
+		// - grep for failure
+		if match {
+			fmt.Printf("log file: %s\n", fullpath)
+
+			var count int
+			h := fnv.New64a()
+			io.WriteString(h, fullpath)
+			pathID := h.Sum64()
+
+			statement, err := db.Prepare("SELECT COUNT(*) FROM visited where pathId = ?")
+			if err != nil {
+				panic(err)
+			}
+			// [golang/go/issues/6113] we can't use uint64 with the high bit set
+			// but we can cast it and store as an int64 without data loss
+			err = statement.QueryRow(int64(pathID)).Scan(&count)
+			if err != nil {
+				panic(err)
+			}
+			if count > 1 {
+				panic(err)
+			}
+			// we've found this log already, skip next steps
+			if count == 1 {
+				fmt.Printf("  link already there: %s\n", fullpath)
+				return
+			}
+
+			statement, err = db.Prepare("INSERT INTO visited (pathID) VALUES (?)")
+			if err != nil {
+				panic(err)
+			}
+			_, err = statement.Exec(int64(pathID))
+			if err != nil {
+				panic(err)
+			}
+
+			resp, err := http.Get(e.Request.AbsoluteURL(link))
+			if err != nil {
+				panic(err)
+			}
+			defer resp.Body.Close()
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				panic(err)
+			}
+
+			if strings.Contains(string(body[:]), "error") {
+				fmt.Printf("> error found for link %s", link)
+				// TODO notify everyone who subscribed
+			} else {
+				fmt.Printf("< error not found for link %s", link)
+			}
+		}
+
 		c.Visit(e.Request.AbsoluteURL(link))
 	})
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println("Finished", r.Request.URL)
-	})
+
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
 	})
