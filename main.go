5c27728dc3dd38e227ed378b947f887340efcbd6
Lorenzo Manacorda <lorenzo@mailbox.org>
2024-08-02 08:53:17 +0200

    split out to visitLog function

--- main.go
+++ main.go
@@ -62,55 +62,10 @@ func main() {
 		// - download file
 		// - grep for failure
 		if match {
-			fullpath := e.Request.AbsoluteURL(link)
-			components := strings.Split(fullpath, "/")
-			pkg := components[len(components)-2]
-			date := strings.Trim(components[len(components)-1], ".log")
-			fmt.Printf("pkg: %s; date: %s\n", pkg, date)
-
-			var count int
-			statement, err := db.Prepare("SELECT COUNT(*) FROM visited where package = ? AND date = ?")
-			if err != nil {
-				panic(err)
-			}
-			err = statement.QueryRow(pkg, date).Scan(&count)
-			if err != nil {
-				panic(err)
-			}
-			// we've found this log already, skip next steps
-			if count == 1 {
-				fmt.Printf("  log already there: %s\n", fullpath)
-				return
-			}
-
-			resp, err := http.Get(e.Request.AbsoluteURL(link))
-			if err != nil {
-				panic(err)
-			}
-			defer resp.Body.Close()
-
-			body, err := io.ReadAll(resp.Body)
-			if err != nil {
-				panic(err)
-			}
-
-			var hasError bool
-			if strings.Contains(string(body[:]), "error") {
-				// fmt.Printf("> error found for link %s\n", link)
-				hasError = true
-				// TODO notify everyone who subscribed
-			}
-			// we haven't seen this log yet, so add it to the list of seen ones
-			statement, _ = db.Prepare("INSERT INTO visited (package, date, error) VALUES (?, ?, ?)")
-			_, err = statement.Exec(pkg, date, hasError)
-			if err != nil {
-				panic(err)
-			}
-
+			visitLog(link, e, db)
+		} else {
+			c.Visit(e.Request.AbsoluteURL(link))
 		}
-
-		// fmt.Printf("about to visit %s\n", e.Request.AbsoluteURL(link))
-		c.Visit(e.Request.AbsoluteURL(link))
 	})
 
 	c.OnError(func(r *colly.Response, err error) {
@@ -118,4 +73,52 @@ func main() {
 	})
 
 	c.Visit(url)
+}
+
+func visitLog(link string, e *colly.HTMLElement, db *sql.DB) {
+	fullpath := e.Request.AbsoluteURL(link)
+	components := strings.Split(fullpath, "/")
+	pkg := components[len(components)-2]
+	date := strings.Trim(components[len(components)-1], ".log")
+	fmt.Printf("pkg: %s; date: %s\n", pkg, date)
+
+	var count int
+	statement, err := db.Prepare("SELECT COUNT(*) FROM visited where package = ? AND date = ?")
+	if err != nil {
+		panic(err)
+	}
+	err = statement.QueryRow(pkg, date).Scan(&count)
+	if err != nil {
+		panic(err)
+	}
+	// we've found this log already, skip next steps
+	if count == 1 {
+		fmt.Println("  skipping")
+		return
+	}
+
+	resp, err := http.Get(e.Request.AbsoluteURL(link))
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var hasError bool
+	if strings.Contains(string(body[:]), "error") {
+		// fmt.Printf("> error found for link %s\n", link)
+		hasError = true
+		// TODO notify everyone who subscribed
+	}
+	// we haven't seen this log yet, so add it to the list of seen ones
+	statement, _ = db.Prepare("INSERT INTO visited (package, date, error) VALUES (?, ?, ?)")
+	_, err = statement.Exec(pkg, date, hasError)
+	if err != nil {
+		panic(err)
+	}
+
 }
