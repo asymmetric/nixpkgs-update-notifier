f78657001f7a674a209cb3ad322e5c29846b1904
Lorenzo Manacorda <lorenzo@mailbox.org>
2024-08-02 17:23:38 +0200

    flake

--- flake.nix
+++ flake.nix
@@ -37,17 +37,9 @@
             # in the Nix store.
             src = ./.;
 
-            # This hash locks the dependencies of this package. It is
-            # necessary because of how Go requires network access to resolve
-            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
-            # details. Normally one can build with a fake hash and rely on native Go
-            # mechanisms to tell you what the hash should be or determine what
-            # it should be "out-of-band" with other tooling (eg. gomod2nix).
-            # To begin with it is recommended to set this, but one must
-            # remember to bump this hash when your dependencies change.
-            # vendorHash = pkgs.lib.fakeHash;
+            vendorHash = "sha256-y+wRag3PH43MHTIBNcVA6QIqNSkjAv9suE8xZ7yWB0A=";
 
-            vendorHash = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+            tags = [ "goolm" ];
           };
         });
 
